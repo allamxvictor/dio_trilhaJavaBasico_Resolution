e23bc4979c7aa55d80b38934789f786151ea5176
Allan Victor <allamxvictorx@gmail.com>
2024-08-11 01:26:13 -0300

    Commit inicial.

---
+++ src/edu/dio/desafios/ContaTerminal.java
@@ -0,0 +1,46 @@
+package edu.dio.desafios;
+
+public class ContaTerminal {
+	private int numeroDaConta;
+	private String agencia;
+	private String nomeDoCliente;
+	private double saldo;
+	
+	public ContaTerminal() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getNumeroDaConta() {
+		return numeroDaConta;
+	}
+	
+	public void setNumeroDaConta(int numeroDaConta) {
+		this.numeroDaConta = numeroDaConta;
+	}
+	
+	public String getAgencia() {
+		return agencia;
+	}
+	
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+	
+	public String getNomeDoCliente() {
+		return nomeDoCliente;
+	}
+	
+	public void setNomeDoCliente(String nomeDoCliente) {
+		this.nomeDoCliente = nomeDoCliente;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+}
